633d9d0fa3db020fa2c5256bbb39205efe6748eb
Leyre Labarga <69302916+llabarga@users.noreply.github.com>
2021-10-06 12:49:29 +0200

    Add files via upload
    First day! Introduction to R

---
+++ scriptSobreR.R
@@ -0,0 +1,16 @@
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#Script sobre uso básico de R
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+#Si me pongo encima de una instrucción y hago cntrl+intro se ejecuta
+#únicamente esa instrucción
+a<-2+3
+
+b<-a+a
+
+c<-18
+
+
+rm(list=ls())
+
+      
